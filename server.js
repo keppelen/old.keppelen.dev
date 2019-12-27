15dd3179a29bc3cce90ba2ea400f478242713221
Keppelen <keppelen.as@gmail.com>
2019-12-26 21:06:31 -0300

    remove zeit config

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const template = (html, style) => `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Giovanni Keppelen</title>
+        <meta name="description" content="Giovanni Keppelen - Manager Front-end Enginner at Plurall" />
+        <title>Giovanni Keppelen | Manager Front-end Enginner at Plurall</title>
         <style>${style}</style>
     </head>
     <body>
